8a892deae8fb7d88efd02cc3e0e9483ae21b102f
agerlac3 <agerlac3@smail.uni-koeln.de>
2021-06-21 11:28:34 +0200

    added some comments

--- code/split_to_paragraph/splitter.py
+++ code/split_to_paragraph/splitter.py
@@ -6,12 +6,15 @@ lösche Zeilen, die keine nicht-alphanumerischen Zeichen enthalten (bspw. nur So
 merge die List-items zsm
 merge die Zeilen, bei denen die erste Zeile nicht mit einem Punkt endet und die zweite nicht mit uppercase char beginnt oder die ein Jobtitel ist
 """
+
+# HIER DIE RELATIONSSHIPS EINBAUEN !!! ein object ist related to list of paragraphs die jeweils einer classify unit hinzugefügt werden!!!
+
 # sowas wie wie main, kann vllt in init
 def jobads_to_paragraphs(jobads: list):
     for obj in jobads:
         split_at_empty_line(obj)
 
-
+# Returns list of paragraphs per object
 def split_at_empty_line(obj: object) -> list:
     list_paragraphs = obj.content.split("\n\n")
     return list_paragraphs
