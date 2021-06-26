4e56ff93edd4fcc2722079e1adeb128f6219ad45
agerlac3 <agerlac3@smail.uni-koeln.de>
2021-06-26 15:07:19 +0200

    implemented classifyunit functions. still work todo

--- code/prepare_classifyunits/__init__.py
+++ code/prepare_classifyunits/__init__.py
@@ -1,10 +1,15 @@
 """Script to split jobads into paragraphs and generate classifyunits for each paragraphs."""
 
 # ## Imports
+from re import I
 from . import converter
 from database import session
 from ORM_structure import orm
 from ORM_structure.models import ClassifyUnits
+from itertools import zip_longest as izip
+from itertools import tee, islice, chain
+import sys
+
 
 def generate_classifyunits(jobad: object):
     """ Function manages the preparation for the textclassification. Therefore classifyunits are needed and will be generated in this step.
@@ -23,28 +28,23 @@ def generate_classifyunits(jobad: object):
 
     # 1. Split the jobad texts (content) and receive a list of paragraphs for each jobad
     list_paragraphs = __get_paragraphs(jobad)
-    # 2. Clean each paragraph
+
+    # 2. Clean each paragraph and merge ListItems and WhatBelongsTogether
+    list_paragraphs = __clean_paragraphs(list_paragraphs)
+
     for para in list_paragraphs:
-        para = __clean_paragraphs(para)
         # 3. Make feature units
         fu = __get_featureunits(para)
-        if fu != '':
-            # 4. Make featurevectors
-            fv = __get_featurevectors(fu)
 
-            # 5. Add whitespace free paragraph, default classID, featureunit (ngram list) and featurevector to classify unit
-            cu = ClassifyUnits(paragraph=para, classID=0, featureunit=fu, featurevector=fv)
-            
-            # 6. Connect the cu (classifyunit) as a child to its parent (jobad)
-            jobad.children.append(cu)
-            
-            # ## Some Print-Statements for checking
-            # prints the related parent id for a child
-            # print(cu.parent.id)
-            # prints each child with the classifyunits id and the related parent id
-            #print(cu)
-        else:
-            pass
+        # 4. Make featurevectors
+        fv = __get_featurevectors(fu)
+
+        # 5. Add whitespace free paragraph, default classID, featureunit (ngram list) and featurevector to classify unit
+        cu = ClassifyUnits(paragraph=para, classID=0, featureunit=fu, featurevector=fv)
+        
+        # 6. Connect the cu (classifyunit) as a child to its parent (jobad)
+        jobad.children.append(cu)
+
 
 
 def __get_paragraphs(jobad: object) -> list:
@@ -52,20 +52,64 @@ def __get_paragraphs(jobad: object) -> list:
     #for jobad in jobads:
     # 1. Split the jobad texts (content) and receive a list of paragraphs for each jobad
     list_paragraphs = converter.split_at_empty_line(jobad)
+    
+    # Remove whitespaces
+    list_paragraphs = converter.remove_whitespaces(list_paragraphs)
+
     return list_paragraphs
 
 
-def __clean_paragraphs(para: str) -> str:
-    # Remove spaces at the beginning and at the end of the string
-    para = converter.remove_whitespaces(para)
-    return para
+def __clean_paragraphs(list_paragraphs: list) -> list:
+
+    # ################### !!! Merge ListIitems nochmal in funktion packen
+    cleaned_merged = list()
+    i = 1
+    list_paragraphs.insert(0, '')
+    list_paragraphs.append('')
+    previous_to_remember = ''
+
+    while i < len(list_paragraphs):
+        # set variables to compare
+        para = list_paragraphs[i]
+        previous = list_paragraphs[i-1]
+
+        # Merge Listitems together
+        para, previous_to_remember = converter.merge_listitems(previous, para, previous_to_remember)
+
+        cleaned_merged.append(para)
+        i += 1
+    #sys.exit()
+    cleaned_merged = list(filter(None, cleaned_merged))
+
+    # #################### !!! Merge what belongs together nochmal in funktion packen
+    # TODO: Merge what belongs together (startswith uppercase, endswith period, is jobtitle)
+    i = 1
+    cleaned_merged.insert(0, '')
+    cleaned_merged.append('')
+    previous_to_remember = ''
+    belongs = []
+    while i < len(cleaned_merged):
+        # set variables to compare
+        para = cleaned_merged[i]
+        previous = cleaned_merged[i-1]
+
+        # Merge Listitems together
+        para, previous_to_remember = converter.merge_whatbelongstogether(previous, para, previous_to_remember)
+
+
+        belongs.append(para)
+        i += 1
+
+    belongs = list(filter(None, belongs))
+    return belongs
 
 # TODO: Rückgabe später sollte kein str sein sonern eine list of strings
 def __get_featureunits(para: str) -> str:
-    # Remove all non alpha-numerical characters
-    fu = converter.replace(para)
     # Ignore all paragraphs without any content
-    if fu != '':
+    if para != '':
+        # Remove all non alpha-numerical characters
+        fu = converter.replace(para)
+
         # TODO: FEATUREUNIT  
         # normalize, stem, filterSW, nGrams, continousNGrams
         # die fus sollten eine liste bestehend aus strings (den ngrammen sein pro cu eine liste)
--- code/prepare_classifyunits/converter.py
+++ code/prepare_classifyunits/converter.py
@@ -2,6 +2,8 @@
 
 # ## Imports
 import re
+from sys import prefix
+from typing import Union
 
 
 # Returns list of paragraphs per object
@@ -9,8 +11,82 @@ def split_at_empty_line(jobad: object) -> list:
     return jobad.content.split("\n\n")
 
 # Remove spaces at the beginning and at the end of the string
-def remove_whitespaces(para: str) -> str:
-    return para.strip()
+def remove_whitespaces(list_paragraphs: list) -> list:
+    # TODO: Allgemeineren Ausdruck finden, um alle Einrückungen in den Zeilen zu löschen.
+    cleaned_paralist = list()
+    for para in list_paragraphs:
+        newpara = []
+        newstring = ''
+        parasplit = para.split('\n')
+        for line in parasplit:
+            line = line.strip()
+            newpara.append(line + '\n')
+        cleaned_paralist.append((newstring.join(newpara)).strip())
+    return cleaned_paralist
+
+# Merge Listitems together
+def merge_listitems(previous: str, para: str, previous_to_remember: str) -> Union[str, str]:
+    # If-condition to prevent duplicated entries in output
+    if previous_to_remember.__contains__(previous):
+        previous, previous_to_remember = __check_for_duplicates(previous, previous_to_remember)
+        return previous, previous_to_remember
+    # Check if two paragraphs contain the required list characters and join them if true.
+    else:
+        previous, previous_to_remember = __isListItem(previous, para, previous_to_remember)
+        return previous, previous_to_remember
+
+# Check if two paragraphs contain the required list characters and join them if true.
+def __isListItem(previous, para, previous_to_remember):
+    # Regex for list items at the end of a paragraph
+    regex_previous = re.compile(r"(\n(\s)*((-\*|-|\*|\d(\.|\\)|\.\\)(\s)*)(\w)+$)")
+    # Regex for list items at the beginning of a paragraph
+    regex_para = re.compile(r"(^(\s)*((-\*|-|\*|\d(\.|\\)|\.\\)(\s)*)\w*)")
+    if regex_previous.search(previous) and regex_para.search(para):
+        previous = "\n".join([previous, para])
+        previous_to_remember = previous
+        # return changed previous to write in output
+        return previous, previous_to_remember
+    else:
+        # return unchanged previous to write in output
+        return previous, previous_to_remember
+
+
+# Check if the previous joined paragraph is used again. 
+# If-condition to prevent duplicated entries in output
+def __check_for_duplicates(previous, previous_to_remember):
+    previous = ''
+    previous_to_remember = ''
+    return previous, previous_to_remember
+
+
+def merge_whatbelongstogether(previous, para, previous_to_remember):
+    if previous_to_remember.__contains__(previous):
+        previous, previous_to_remember = __check_for_duplicates(previous, previous_to_remember)
+        return previous, previous_to_remember
+    # Check if two paragraphs contain the required list characters and join them if true.
+    else:
+        previous, previous_to_remember = __BelongsItem(previous, para, previous_to_remember)
+        return previous, previous_to_remember
+
+def __BelongsItem(previous, para, previous_to_remember):
+    if previous != '' and previous_to_remember != '':
+        if previous.endswith('.') and (para[0].isupper() or __looksLikeJobTitle(para)):
+            print(previous)
+            print(para)
+            previous = "\n".join([previous, para])
+            previous_to_remember = previous
+            # return changed previous to write in output
+            return previous, previous_to_remember
+        else:
+            return previous, previous_to_remember
+    else:
+        # return unchanged previous to write in output
+        return previous, previous_to_remember
+
+def __looksLikeJobTitle(para):
+    regex_jobtitle = re.compile(r"^.*\w+/-?\w+.*$")
+    return regex_jobtitle.match(para)
+
 
 # elegantere Lösung finden? vllt gensim simple preprocessing für alles in einem (inkl remove whitespaces)
 def replace(para) -> str:
