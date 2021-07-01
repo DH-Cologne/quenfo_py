76d1a57d182277edc93fe5b16798e8223c2da89d
agerlac1 <agerlac1@uni-koeln.de>
2021-07-01 10:00:06 +0200

    test3

--- code/ORM_structure/orm.py
+++ code/ORM_structure/orm.py
@@ -27,7 +27,7 @@ def get_jobads(session: Session) -> list:
     ClassifyUnits.__table__.create(engine) """
     
     job_ads = session.query(JobAds).limit(500).all()
-    # delete the handles from jobads to classifyunits
+    # delete the handles from jobads to classif
     session.query(ClassifyUnits).delete()
     session.commit()
     
