88611a9394aba3641e156b42ee63b1f96176ac98
ChristineSchaefer <82021777+ChristineSchaefer@users.noreply.github.com>
2021-07-07 11:03:10 +0200

    remove django from quenfo_py

--- code/webapp/quenfowebapp/configuration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-#namespace für url
-app_name = 'configuration'
-urlpatterns = [
-    #leere Anführungszeichen heißt, dass das bei Aufruf der App gesetzt wird
-    path('', views.index, name='index'),
-]
