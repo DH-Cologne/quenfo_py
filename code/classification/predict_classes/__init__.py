668db4f8dc2be4ed19e4c09c1f0a3db32549c401
agerlac1 <agerlac1@uni-koeln.de>
2021-08-24 19:04:11 +0200

    added regex prediction file still empty

--- code/classification/predict_classes/__init__.py
+++ code/classification/predict_classes/__init__.py
@@ -1,9 +1,11 @@
 from . import knn_predictor
+
 def start_prediction(jobad, model):
 
     for cu in jobad.children:
 
-        clf = model.model_knn
-        predicted = knn_predictor.gen_classes(cu.featurevector, clf)
+        predicted = knn_predictor.gen_classes(cu.featurevector, model.model_knn)
+        # hier dann auch den regex predictor reinmachen und dann direkt vergleichen!
+        
         cu.set_classID(predicted[0].replace('\n', ''))
         print(cu, predicted[0].replace('\n', ''))
