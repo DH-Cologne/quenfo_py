6052a964a79a0f8f91c427f130250d64b5d36096
agerlac1 <agerlac1@uni-koeln.de>
2021-09-20 15:04:46 +0200

    cleaned additional scripts

--- additional_scripts/make_sql_traindata.py
+++ additional_scripts/make_sql_traindata.py
@@ -1,3 +1,4 @@
+# Imports
 import pandas as pd
 import sqlite3
 from sqlite3 import Error
@@ -12,43 +13,39 @@ def load_input_data():
     # create a database connection
     with open(input_path, 'r') as f:
         filelines = f.readlines()
-    #print(filelines)
     return filelines
 
 # Gets filelines and transfers content to dataframe
 def transfer_lines_to_df(filelines):
-    jahrgang = 0
     postingID = str
     classID = 0
     line_content = []
     all_rows = []
 
     for line in filelines:
-        # Wenn eine Zeile 2 oder 3 Tabs hat, handelt es sich um die Kopfzeile mit den Informationen: postingID, Zeilennr. und classID
+        # If line is divided by 2 or 3 tabs, it is the header (with the important information): postingID, Zeilennr. und classID
         if line.split('\t').__len__() == 3 or line.split('\t').__len__() == 2:
             splitted = line.split('\t')
-            #print(line)
             classID = splitted[2] 
             postingID = splitted[0]
             zeilennr = splitted[1]
-        # Alle anderen Zeilen, die keine leeren Zeilen sind, sind content-Informationen
+        # All other lines (that are not empty) are content-information
         elif line != '\n':
-            #print(line)
             line_content.append(line)
-        # Das sind die leeren Zeilen, die zur Unterteilung zwischen den einzelnen Stellenanzeigen benutzt werden
+        # These are the empty lines that are used to subdivide the individual job advertisements
         else:
-            # Füge die einzelnen Content-Sätze zu einem Text zsm
+            # join lines to one content-text
             content = ''.join(line_content)
-            # Setze line_content für die nächste Jobad zurück
+            # Reset line_content for each jobad
             line_content = []
-            # Erstelle mit allen gesammelten Infos für die aktuelle Jobad einen Eintrag in der Liste
+            # Create an entry in the list with all the information you have collected for the current job
             list_df_prepraration = [postingID, zeilennr, classID, content]
-            # Lege Eintrag für die Jobad in großer Liste an
+            # Create an entry for the Jobad in a large list
             all_rows.append(list_df_prepraration)
-            # Setze die Jobad liste zurück
+            # reset list
             list_df_prepraration= []
  
-    # Generiere das df mit den gesammelten Infos und folgenden Spalten:
+    # Genearte df with all rows in specific columns
     df = pd.DataFrame(all_rows, columns=['postingId', 'zeilennr', 'classID', 'content'])
     return df
 
@@ -58,7 +55,6 @@ def writer(df_train):
     
     # Name of database table
     key = 'traindata'
-
     df_train['index'] = df_train.index
 
     # make connection
@@ -81,7 +77,7 @@ def writer(df_train):
     data.to_sql(key, conn_temp, if_exists = 'replace', chunksize = 1000, index = False)
     conn_temp.close()
 
-# Main Methode
+# Main Method
 def main():
     filelines = load_input_data()
     df = transfer_lines_to_df(filelines)
