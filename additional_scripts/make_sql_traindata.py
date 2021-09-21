e7794335b3dee8626830740a7b4a36056f934881
ChristineSchaefer <82021777+ChristineSchaefer@users.noreply.github.com>
2021-09-21 14:37:06 +0200

    remoce merge errors

--- additional_scripts/make_sql_traindata.py
+++ additional_scripts/make_sql_traindata.py
@@ -5,8 +5,8 @@ from sqlite3 import Error
 import os
 
 # Paths definieren
-input_path = 'C:\\Users\\Christine\\Documents\\Qualifikationsentwicklungsforschung\\quenfo\\quenfo_v1_1_3\\quenfo_data\\resources\\classification\\trainingSets\\trainingdata_anonymized.tsv'
-output_path = 'C:\\Users\\Christine\\Documents\\Qualifikationsentwicklungsforschung\\quenfo\\quenfo_v1_1_3\\quenfo_data\\sqlite\\orm\\traindata_sql.db'
+input_path = '..\\..\\quenfo_data\\resources\\classification\\trainingSets\\trainingdata_anonymized.tsv'
+output_path = '..\\..\\quenfo_v1_1_3\\quenfo_data\\sqlite\\orm\\traindata_sql.db'
 
 
 # Loads Input Data (Trainingdata) and readlines
@@ -14,10 +14,6 @@ def load_input_data():
     # create a database connection
     with open(input_path, 'r', encoding="utf-8") as f:
         filelines = f.readlines()
-<<<<<<< HEAD:additional_scripts/make_sql_traindata.py
-=======
-    # print(filelines)
->>>>>>> 8556f489fb9eeb290f76787d221be24aebd2bf3b:make_sql_traindata.py
     return filelines
 
 
@@ -29,27 +25,15 @@ def transfer_lines_to_df(filelines):
     all_rows = []
 
     for line in filelines:
-<<<<<<< HEAD:additional_scripts/make_sql_traindata.py
+
         # If line is divided by 2 or 3 tabs, it is the header (with the important information): postingID, Zeilennr. und classID
         if line.split('\t').__len__() == 3 or line.split('\t').__len__() == 2:
             splitted = line.split('\t')
             classID = splitted[2] 
-=======
-        # Wenn eine Zeile 2 oder 3 Tabs hat, handelt es sich um die Kopfzeile mit den Informationen: postingID,
-        # Zeilennr. und classID
-        if line.split('\t').__len__() == 3 or line.split('\t').__len__() == 2:
-            splitted = line.split('\t')
-            # print(line)
-            classID = splitted[2]
->>>>>>> 8556f489fb9eeb290f76787d221be24aebd2bf3b:make_sql_traindata.py
             postingID = splitted[0]
             zeilennr = splitted[1]
         # All other lines (that are not empty) are content-information
         elif line != '\n':
-<<<<<<< HEAD:additional_scripts/make_sql_traindata.py
-=======
-            # print(line)
->>>>>>> 8556f489fb9eeb290f76787d221be24aebd2bf3b:make_sql_traindata.py
             line_content.append(line)
         # These are the empty lines that are used to subdivide the individual job advertisements
         else:
@@ -61,17 +45,10 @@ def transfer_lines_to_df(filelines):
             list_df_prepraration = [postingID, zeilennr, classID, content]
             # Create an entry for the Jobad in a large list
             all_rows.append(list_df_prepraration)
-<<<<<<< HEAD:additional_scripts/make_sql_traindata.py
             # reset list
             list_df_prepraration= []
  
-    # Genearte df with all rows in specific columns
-=======
-            # Setze die Jobad liste zurück
-            list_df_prepraration = []
-
-    # Generiere das df mit den gesammelten Infos und folgenden Spalten:
->>>>>>> 8556f489fb9eeb290f76787d221be24aebd2bf3b:make_sql_traindata.py
+    # Generate df with all rows in specific columns
     df = pd.DataFrame(all_rows, columns=['postingId', 'zeilennr', 'classID', 'content'])
     return df
 
@@ -104,12 +81,8 @@ def writer(df_train):
     data.to_sql(key, conn_temp, if_exists='replace', chunksize=1000, index=False)
     conn_temp.close()
 
-<<<<<<< HEAD:additional_scripts/make_sql_traindata.py
-# Main Method
-=======
 
 # Main Methode
->>>>>>> 8556f489fb9eeb290f76787d221be24aebd2bf3b:make_sql_traindata.py
 def main():
     filelines = load_input_data()
     df = transfer_lines_to_df(filelines)
