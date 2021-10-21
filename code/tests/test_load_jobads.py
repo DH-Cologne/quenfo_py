dab2d81f6ff0c4c1edfbe34ab2bb316a170cfd66
agerlac1 <agerlac1@uni-koeln.de>
2021-10-21 10:33:45 +0200

    added tests and docu for dupli

--- code/tests/test_load_jobads.py
+++ code/tests/test_load_jobads.py
@@ -1,27 +1,34 @@
-""" import unittest
-
+import unittest
 import orm_handling.orm
-from database import connection
+import database
 from orm_handling.models import JobAds, ClassifyUnits
+import configuration
 
-
+# First preparation-steps to execute following Tests
+method_args = {'input_path': 'C:\\Users\\Anne\\Desktop\\Quenfo\\quenfo_py_data\\sqlite\\orm\\text_kernel_orm_2018_03.db', 'db_mode': 'append'}
+configuration.set_config(method_args)
+database.set_input_conn()
+database.set_train_conn()
+start_pos = 0
+ 
+### TODO: Die Funktionen aus orm.py austesten. Die erste hier test_table_data kann für get_jobads genutzt wrden.
 class TestGetJobAds(unittest.TestCase):
     def test_table_data(self):
-        job_ads = connection.session.query(JobAds)
+        job_ads = database.session.query(JobAds)
         self.assertIsNotNone(job_ads, "Table from database is empty.")
 
     def test_create_new_table(self):
-        if connection.session.query(ClassifyUnits) is None:
-            orm_handling.orm.get_jobads()
-            self.assertIsNotNone(connection.session.query(ClassifyUnits), "Table classify_units in database does not "
+        if database.session.query(ClassifyUnits) is None:
+            orm_handling.orm.get_jobads(start_pos)
+            self.assertIsNotNone(database.session.query(ClassifyUnits), "Table classify_units in database does not "
                                                                           "exist.")
 
     def test_type_of_jobads(self):
-        output = orm_handling.orm.get_jobads()
+        output = orm_handling.orm.get_jobads(start_pos)
         self.assertIsInstance(output, list)
         if not any(isinstance(item, JobAds) for item in output):
             print("List is not type JobAds.")
 
 
 if __name__ == '__main__':
-    unittest.main()"""
+    unittest.main()
